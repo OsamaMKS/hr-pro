bc8dff8dc4867d467abed1772df07b6b803f8a2c
Osama Sider <osamamksider@hotmail.com>
2020-02-09 12:12:50 +0200

    HR-program

--- hr_pro.py
+++ hr_pro.py
@@ -2,14 +2,12 @@ from datetime import datetime
 
 class Employee:
    #Employee class here
-   Emp=[]
    def __init__(self,name,age,salary,employment_year):
        self.name = name
        self.age = age
        self.salary = salary
        self.employment_year = employment_year
 
-       self.Emp.append({"name":self.name,"age":self.age,"salary":self.salary,"Work Experience":Employee.get_working_years(self)})
 
    def get_working_years(self):
 
@@ -20,15 +18,14 @@ class Employee:
 
 class Manager(Employee):
     #Manager class here
-    Mang=[]
+
     def __init__(self,name,age,salary,employment_year,bonus_percentage):
-        #super().__init__(self,name,age,salary,employment_year)
+        #super().__init__(name,age,salary,employment_year)
         self.name = name
         self.age = age
         self.salary = salary
         self.employment_year = employment_year
         self.bonus_percentage = bonus_percentage
-        self.Mang.append({"name":self.name,"age":self.age,"salary":self.salary,"Work Experience":Employee.get_working_years(self),"Bouns":Manager.get_bonus(self)})
 
     def get_bonus(self):
         x = self.salary * self.bonus_percentage
@@ -37,46 +34,50 @@ class Manager(Employee):
 
 def main():
 
-    print("Options:")
-    print("1. Show Employees")
-    print("2. Show Managers")
-    print("3. Add An Employee")
-    print("4. Add A Manager")
-    print("5. Exit")
-    print()
 
-
-    x = int(input("What would you like to do?"))
-    if x == 1:
-        for i in Employee.Emp :
-            print(i)
-        main()
-    elif x == 2 :
-        for i in Manager.Mang:
-            print(i)
-        main()
-    elif x == 3 :
-        name = input("Name: ")
-        age = int(input("Age: "))
-        salary = float(input("Salary: "))
-        employment_year = int(input("Employment year: "))
-        Employee(name,age,salary,employment_year)
-        print("Employee added succesfully")
+    emp=[]
+    mang=[]
+    x=""
+    while x != 5:
+        print("Options:")
+        print("1. Show Employees")
+        print("2. Show Managers")
+        print("3. Add An Employee")
+        print("4. Add A Manager")
+        print("5. Exit")
         print()
-        main()
+        x = int(input("What would you like to do?"))
+        if x == 1:
+            for i in emp :
+                print(i)
 
-    elif x == 4 :
-        name = input("Name: ")
-        age = int(input("Age: "))
-        salary = float(input("Salary: "))
-        employment_year = int(input("Employment year: "))
-        bouns = float(input("Bouns Percentage: "))
-        Manager(name,age,salary,employment_year,bouns)
-        print("Manager added succesfully")
-        print()
-        main()
-    elif x == 5:
-         print()
+        elif x == 2 :
+            for i in mang:
+                print(i)
+
+        elif x == 3 :
+            name = input("Name: ")
+            age = int(input("Age: "))
+            salary = float(input("Salary: "))
+            employment_year = int(input("Employment year: "))
+            r = Employee(name,age,salary,employment_year)
+            emp.append({"name":str(r.name),"age":r.age,"salary":r.salary,"Work Experience":Employee.get_working_years(r)})
+            print("Employee added succesfully")
+            print()
+
+        elif x == 4 :
+            name = input("Name: ")
+            age = int(input("Age: "))
+            salary = float(input("Salary: "))
+            employment_year = int(input("Employment year: "))
+            bouns = float(input("Bouns Percentage: "))
+            t = Manager(name,age,salary,employment_year,bouns)
+            mang.append({"name":t.name,"age":t.age,"salary":t.salary,"Work Experience":Employee.get_working_years(t),"Bouns":Manager.get_bonus(t)})
+            print("Manager added succesfully")
+            print()
+
+        elif x == 5:
+            break
 
 if __name__ == '__main__':
     main()
